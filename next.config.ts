cd3607ddda1351247d950904decd84ccc832ff80
hangz <hangz@rings.net.cn>
2025-10-15 19:18:57 +0800

    update: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export",
   distDir: ".next",
   images: {
     domains: ["localhost"],
